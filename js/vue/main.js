27e9b6d1a4799356c3e5d82054a1eec3d0d1e754
McxCZE <milan.cihak@brouzdej.net>
2025-05-13 21:47:34 +0200

    VueJS - Implementace

--- js/vue/main.js
+++ js/vue/main.js
@@ -1,25 +1,67 @@
 import { createApp, onMounted, onUnmounted, ref, reactive } from 'vue'
-import Navbar from './navbar.js'
-
-// My, neochotní, vedeni neznalými, děláme nemožné pro nevděčné. Tolik jsme toho dokázali, tak dlouho, 
-// s tak málem, že jsme nyní kvalifikovaní dělat cokoli z ničeho.
 
 const MainApp = {
-    components: {
-        Navbar
-    },
+    components: {},
     setup() {
-        // const something = ref('home')
+        const activePage = ref('home')
+
+        const navItems = [
+            { name: 'home', label: 'Domů' },
+            { name: 'about-us', label: 'O nás' },
+            { name: 'products', label: 'Produkty' },
+            { name: 'inspiration', label: 'Inspirace' },
+            { name: 'contact', label: 'Kontakt' },
+            { name: 'reviews', label: 'Recenze', disabled: true },
+        ]
 
         return {
+            activePage,
+            navItems
         }
     },
     template: /*html*/`
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <i class="bi bi-pc-display-horizontal"></i> - Míra Kouduje
+                </a>
+                <button class="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNav" 
+                    aria-controls="navbarNav" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item" v-for="item in navItems" :key="item.name">
+                            <a class="nav-link"
+                            href="#"
+                            @click.prevent="activePage = item.name"
+                            :class="{ active: activePage === item.name, disabled: item.disabled }">
+                            {{ item.label }}
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
         <div class="container">
             <div class="row">
-                <Navbar>
+                <div class="col">
+                    <h1 class="display-4 fw-bold">Vítejte na mém webu</h1>
+                    <p class="lead">Tady najdete projekty, nápady a věci, které mě baví kódovat.</p>
+                    <a href="#" class="btn btn-primary btn-lg mt-3">Zjistit víc</a>
+                </div>
             </div>
             <div class="row">
+
             </div>
         </div>  
     `
