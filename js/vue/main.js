d0c6287d811eeaf1e83dc28bd766835bc7573fdb
McxCZE <milan.cihak@brouzdej.net>
2025-05-13 21:18:27 +0200

    Vue

--- js/vue/main.js
+++ js/vue/main.js
@@ -1,19 +1,27 @@
 import { createApp, onMounted, onUnmounted, ref, reactive } from 'vue'
+import Navbar from './navbar'
 
 // My, neochotní, vedeni neznalými, děláme nemožné pro nevděčné. Tolik jsme toho dokázali, tak dlouho, 
 // s tak málem, že jsme nyní kvalifikovaní dělat cokoli z ničeho.
 
 const MainApp = {
     components: {
+        Navbar
     },
     setup() {
-        const activeTab = ref('home')
+        // const something = ref('home')
 
         return {
         }
     },
     template: /*html*/`
-        <H1>Hello</H1>
+        <div class="container">
+            <div class="row">
+                <Navbar>
+            </div>
+            <div class="row">
+            </div>
+        </div>  
     `
 }
 
